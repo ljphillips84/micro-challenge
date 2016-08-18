6f0e62376a77e3e10445566d56a16ce6bc81a78b
Deaglan Crew <d.crew@liv.ac.uk>
2016-08-18 15:37:46 +0100

    added doc strings to solutions, for consistency.

--- (EASY)_palindrome_checker.py
+++ (EASY)_palindrome_checker.py
@@ -1,13 +1,9 @@
 def is_palindrome(istring):
-<<<<<<< HEAD
+        '''Takes a string as input and returns a boolean
+       (True/False) output dependant on whether the
+       string is a palindrome.
+    '''
     for i in xrange(len(istring)/2):
         if istring[i] != istring[-(i+1)]:
             return False
     return True
-=======
-    '''Takes a string as input and returns a boolean
-       (True/False) output dependant on whether the
-       string is a palindrome.
-    '''
-    return 
->>>>>>> master
