000ac68c3cd31521d8c54e05da96492315ca5bff
Deaglan Crew <d.crew@liv.ac.uk>
2016-08-18 15:32:11 +0100

    Added template functions to master for challenges.

---
+++ (EASY)_palindrome_checker.py
@@ -0,0 +1,6 @@
+def is_palindrome(istring):
+    '''Takes a string as input and returns a boolean
+       (True/False) output dependant on whether the
+       string is a palindrome.
+    '''
+    return 
