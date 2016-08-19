3bdda888a4f0cbbbfbfe5c4df781100818e5d5fe
treharne <R.Treharne@liverpool.ac.uk>
2016-08-19 20:49:11 +0100

    deg_to_rad added

---
+++ EASY/deg_to_rad/deg_to_rad.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sys
+
+def deg_to_rad(deg):
+    '''input angle in degrees
+       return rad
+    '''
+    return 
+
+if __name__ = "__main__":
+    print(deg_to_rad(sys.argv[1]))
