48db082935873d1ab1693bec6fd0f25f7d16c8ec
treharne <R.Treharne@liverpool.ac.uk>
2016-08-20 17:20:06 +0100

    rosenbrock.py added

---
+++ INTERMEDIATE/rosenbrock/rosenbrock.py
@@ -0,0 +1,13 @@
+def rosen(x, y, a=1, b=100):
+    '''
+    Input x and y lists and return Rosenbrock function
+    '''
+    return
+
+def plot3D(func):
+    '''
+    Input 2D function and generate a 3D surface plot
+    '''
+    return
+
+
