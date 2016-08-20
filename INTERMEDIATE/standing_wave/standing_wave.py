eff808a4aede5dc919bc4e8796dec51d4064b14b
treharne <R.Treharne@liverpool.ac.uk>
2016-08-20 10:13:37 +0100

    standing_wave.py added

---
+++ INTERMEDIATE/standing_wave/standing_wave.py
@@ -0,0 +1,10 @@
+'''
+Objectives:
+    - use matplotlib animate to plot a moving sine curve
+    - plot a standing wave
+
+Resources:
+    - http://matplotlib.org/api/animation_api.html#matplotlib.animation.FuncAnimation
+    - Googling this one will help
+'''
+
