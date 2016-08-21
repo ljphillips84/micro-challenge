7ed1b3334eab81a268af7fe04ec6bdaa156addfa
treharne <R.Treharne@liverpool.ac.uk>
2016-08-21 09:16:22 +0100

    splitsville update

--- EASY/splitsville/splitsville.py
+++ EASY/splitsville/splitsville.py
@@ -1,30 +1,44 @@
 '''
 HiPy Micro Challenge: "Splittsville"
-Level: EASY
+Level: INTERMEDIATE
+Pre-requisite: "read_data" (EASY)
 Author: R. Treharne
 Date: 21 Aug 2016
 '''
 
-def read_data(fPath):
+import sys
+import numpy as np
+
+def read_to_col(fPath):
     '''
     read in large data file
     transpose data into columns and return as list of lists
     remember - first column is x data
     '''
-    return 
+
+    return
 
 def split_data(data):
     '''
-    split data into a single x and y column
-    save each column pair to a new file
-    specifiy filename as a three digit integer 
-    (e.g. 001.csv) and autoincrement
+    reads in list of lists containing column data
+    creates dictionary for x values - yields to generator
+    creates dictionary for each column of y data,
+    defines dict key as three digit integer filename
+    (e.g. 001.csv) - yields each y dict to generator
     '''
-    return
+    yield
 
-def save_data(data, sPath):
+def save_data(gen, dPath):
     '''
-    you might want to write a separate function 
-    to save the data
+    reads in generator
+    for each y dict in generator - x,y columns written to file 
+    filename is key of y dict
     '''
+
+if __name__ == "__main__":
+    # arg1 - filename to be split
+    # arg2 - save directory
+    data = read_to_col(sys.argv[1])
+    split = split_data(data)
+    save_data(split, str(sys.argv[2]))
 
