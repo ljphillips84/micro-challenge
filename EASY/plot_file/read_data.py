92f1a5bdc026811fcb0e91083053ed522fc16628
treharne <R.Treharne@liverpool.ac.uk>
2016-08-21 19:32:40 +0100

    read_data.py emptied

--- EASY/plot_file/read_data.py
+++ EASY/plot_file/read_data.py
@@ -15,24 +15,13 @@ def read_to_row(fPath):
     return a list of lists containing row data
     '''
 
-    data = [] # define empty list
 
-    # open file to read
-    with open(fPath, "r") as f:
-        for line in f.readlines(): # read every line and then loop through
-            # split each line into list using comma 
-            # loop through each element in new list and make float
-            # append item to data list
-            data.append([float(x) for x in line.split(",")])
-
-    return data # return the list
+    return 
 
 def read_to_col(fPath):
     '''
     return transposed data as a list of lists containing column data
     '''
-    return np.transpose(read_to_row(fPath)) # just transpose the rows
+    return 
 
-if __name__ == "__main__":
-    print(read_to_row(sys.argv[1]))
 
