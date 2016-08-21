53fe839833fe6600cdbcc4448421137b92b85c66
treharne <R.Treharne@liverpool.ac.uk>
2016-08-21 14:20:19 +0100

    read_data added

---
+++ EASY/read_data/read_data.py
@@ -0,0 +1,26 @@
+'''
+HiPy Micro Challenge: "Read Data"
+Level: EASY 
+Pre-requisite: None
+Author: R. Treharne
+Date: 21 Aug 2016
+'''
+
+import sys
+import numpy as np
+
+def read_to_row(fPath):
+    '''
+    read all lines in data file
+    return a list of lists containing row data
+    '''
+
+    return 
+
+def read_to_col(fPath):
+    '''
+    return transposed data as a list of lists containing column data
+    '''
+    return
+
+
