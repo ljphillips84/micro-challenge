0d9fc78959cd188ffe2266619da55b958961d6c6
treharne <R.Treharne@liverpool.ac.uk>
2016-08-21 07:52:44 +0100

    splitsville added

---
+++ EASY/splitsville/splitsville.py
@@ -0,0 +1,30 @@
+'''
+HiPy Micro Challenge: "Splittsville"
+Level: EASY
+Author: R. Treharne
+Date: 21 Aug 2016
+'''
+
+def read_data(fPath):
+    '''
+    read in large data file
+    transpose data into columns and return as list of lists
+    remember - first column is x data
+    '''
+    return 
+
+def split_data(data):
+    '''
+    split data into a single x and y column
+    save each column pair to a new file
+    specifiy filename as a three digit integer 
+    (e.g. 001.csv) and autoincrement
+    '''
+    return
+
+def save_data(data, sPath):
+    '''
+    you might want to write a separate function 
+    to save the data
+    '''
+
