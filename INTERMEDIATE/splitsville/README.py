52001d66b51132942e15200fdb612d643c98cf78
treharne <R.Treharne@liverpool.ac.uk>
2016-08-21 09:31:33 +0100

    splitsville update

---
+++ INTERMEDIATE/splitsville/README.py
@@ -0,0 +1,14 @@
+# Splitsville
+
+## Objectives
+
+  + Read large, multi-column data file "compound.csv" to a list of lists
+  + Create a generator containing a dictionary for each list (with appropriate keys)
+  + Write all x,y column pair to a new data files. See "example_output.csv"
+
+## Recommended Pre-requisites
+
+  + EASY/read_data
+
+
+
