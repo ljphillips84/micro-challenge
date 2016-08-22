3dec0dd5d3c8284ae975178e6024e060d96a0de1
treharne <R.Treharne@liverpool.ac.uk>
2016-08-22 12:49:56 +0100

    find_prime added

---
+++ EASY/find_prime/README.md
@@ -0,0 +1,17 @@
+# Find Prime
+
+The challenge is designed to get you used to creating, handling and searching lists. 
+
+# Objectives
+
+  + Create a list of 1000 random integers
+  + Sort the list from low to high
+  + Find all the prime numbers in the list
+
+# Pre-requisites
+
+  + None
+
+# Resources
+
+  + None
---
+++ EASY/find_prime/find_prime.py
@@ -0,0 +1,20 @@
+def random_list(N):
+    '''
+    Function returns a list of random integers of length N
+    '''
+    return random_list
+
+def list_sort(input_list, order="lh"):
+    '''
+    Function returns a list of sorted integers
+    List order specified by 'lh' - low to high
+    or 'hl' high to low
+    '''
+    return sorted_list
+
+def find_prime(input_list):
+    '''
+    Function finds all primes in a list of integers
+    and returns sublist containing primes
+    '''
+    return prime_list
